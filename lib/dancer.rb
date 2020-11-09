60dd3d16ef76d16481ac1c82c922b2ff20f6b1c3
Diana Mora <morra.dianna@gmail.com>
2020-11-09 09:21:31 -0500

    commit tree f35c43e3b2c6f8572547136b813651a7f0420db8

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,22 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+extend FancyDance::ClassMethods
+include FancyDance::InstanceMethods
+      
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+# WE CAN CALL THE FOLLOWING:
+# angelina = Dancer.new
+# angelina.twirl
+# // returns "I'm twirling!"
+# angelina.jump
+# // returns "Look how high I'm jumping!"
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,24 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+extend FancyDance::ClassMethods
+include FancyDance::InstanceMethods
+
+attr_accessor :name
+
+    def initialize(name)
+        @name = name 
+    end
+
+end
+
+# WE CAN CALL THE FOLLOWING:
+# buster = Kid.new
+# buster.jump
+# // returns "Look how high I'm jumping!"
+# buster.take_a_bow
+# // returns "Thank you, thank you. It was a pleasure to dance for you all."
+
+  
